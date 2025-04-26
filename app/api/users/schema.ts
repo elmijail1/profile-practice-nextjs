9dec9675de2b70ec7b6bfadc17cab347f256e460
chalupa-bazooka <mteplyshev@gmail.com>
2025-04-26 20:16:43 +0400

    Update emoji & joinedIn - emoji now can have 1-2 chars instead of 1, since emojis are often treated as 2 chars - joinedIn now has a conditional function and can handle strings that contain dates

--- app/api/users/schema.ts
+++ app/api/users/schema.ts
@@ -5,9 +5,11 @@ const userSchema = z.object({
     email: z.string().email(),
     name: z.string().max(20),
     username: z.string().max(20),
-    emoji: z.string().length(1),
+    emoji: z.string().min(1).max(2),
     bgColor: z.array(z.number()).length(3),
-    joinedIn: z.date(),
+    joinedIn: z.preprocess((arg) => {
+        if (typeof arg === "string" || arg instanceof Date) return new Date(arg)
+    }, z.date()),
     aboutMe: z.string().max(100),
     friends: z.array(z.number())
 })
