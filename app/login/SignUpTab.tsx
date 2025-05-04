3eb62762649fe40f1f58aaa327ce54a4da52300f
chalupa-bazooka <mteplyshev@gmail.com>
2025-05-04 09:55:35 +0400

    Update the handleSubmission function to sign in the user

--- app/login/SignUpTab.tsx
+++ app/login/SignUpTab.tsx
@@ -2,6 +2,8 @@
 
 import { useState, useEffect } from "react"
 import ValidationIndicator from "./ValidationIndicator";
+import { signIn, getSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function SignUpTab() {
     const [inputData, setInputData] = useState({ email: "", password: "", passwordRepeat: "" })
@@ -67,11 +69,14 @@ export default function SignUpTab() {
     }
 
     const [isSubmitting, setIsSubmitting] = useState(false)
+    const [error, setError] = useState("")
+    const router = useRouter()
 
     async function handleSubmission(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault()
         setIsSubmitting(true)
 
+        // create a user
         try {
             const response = await fetch(`/api/register`, {
                 method: "POST",
@@ -82,6 +87,7 @@ export default function SignUpTab() {
             })
 
             if (!response.ok) {
+                setError("Error creating user")
                 const errorData = await response.json()
                 console.error("Failed to create a user: ", errorData)
                 return
@@ -91,6 +97,35 @@ export default function SignUpTab() {
             console.log(createdUser)
         } catch (error) {
             console.error("Error while creating a user: ", error)
+        } finally {
+            setIsSubmitting(false)
+        }
+
+        // sign in the user
+        try {
+            const response = await signIn("credentials", {
+                redirect: false,
+                email: inputData.email,
+                password: inputData.password
+            })
+
+            if (!response?.ok) {
+                console.error("Login failed")
+                setError("Invalid email or password")
+                return
+            }
+
+            const session = await getSession()
+
+            if (!session?.user.id) {
+                console.error("Session missing user ID")
+                setError("Session missing user ID")
+                return
+            }
+
+            router.push(`/profile/${session.user.id}`)
+        } catch (error) {
+            console.error("Error while logging in a user: ", error)
         } finally {
             setIsSubmitting(false)
         }
