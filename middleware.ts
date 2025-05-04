7079adb8deacfc78b2fadbc8c2cebc270ad85fa2
chalupa-bazooka <mteplyshev@gmail.com>
2025-05-04 17:23:47 +0400

    Set up middleware
    - does a check at /login: if the user is authenticated, they are redirected to /profile/[id]

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export async function middleware(req: NextRequest) {
+    const token = await getToken({ req })
+
+    const { pathname } = req.nextUrl
+
+    if (pathname === "/login" && token) {
+        return NextResponse.redirect(new URL(`/profile/${token?.id}`, req.url))
+    }
+
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: ["/login"]
+}
