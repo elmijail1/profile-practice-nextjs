10d375550cbf8fd8f87a91b06e7d2d74f2730995
chalupa-bazooka <mteplyshev@gmail.com>
2025-05-04 14:21:55 +0400

    Add logged user highlight to the list
    - also add a try / catch block to the fetcher function
    - add usersLoading state
    - add a conditional for when the length of the users array is 0

--- app/people/page.tsx
+++ app/people/page.tsx
@@ -1,31 +1,43 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { nanoid } from "nanoid";
 import SortByButton from "../components/SortByButton";
 import ListRow from "./ListRow";
 import { sortingOptionsData } from "@/data/sortingOptionsData";
 import { sortBy } from "../../utilities/sorting";
 import type { User } from "../types/user";
+import { useSession } from "next-auth/react";
 
 export default function People() {
+
     const [activeSorting, setActiveSorting] = useState<"joinedIn" | "username" | "name">("joinedIn")
+    const [usersLoading, setUsersLoading] = useState(true)
+    const [users, setUsers] = useState<User[]>([])
+
+    const { data: session, status } = useSession()
+
     const pageColors = {
         sortByText: [200, 80, 60],
         sortByBackground: [0, 0, 100],
     }
 
-    const [users, setUsers] = useState<User[] | undefined>()
     useEffect(() => {
         async function fetchUsers() {
-            const res = await fetch("/api/users")
-            const users = await res.json()
-            setUsers(users)
+            try {
+                const res = await fetch("/api/users")
+                const users: User[] = await res.json()
+                setUsers(users)
+            } catch (error) {
+                console.error("Error fetching users: ", error)
+            } finally {
+                setUsersLoading(false)
+            }
         }
         fetchUsers()
     }, [])
 
-    if (!users) {
+
+    if (usersLoading || status === "loading") {
         return (
             <p>Loading...</p>
         )
@@ -34,38 +46,42 @@ export default function People() {
     return (
         <main>
             <div className="Global__Screen Global__Screen__Blue">
-
                 <div className="Peop__HeaderDiv">
-                    <h1 className="Global__H1__Centered">
-                        People
-                    </h1>
+                    <h1 className="Global__H1__Centered">People</h1>
                 </div>
 
                 <SortByButton
                     activeSorting={activeSorting}
                     setActiveSorting={setActiveSorting}
                     sortingOptionsData={sortingOptionsData}
-                    colors={{ text: pageColors.sortByText, background: pageColors.sortByBackground }}
+                    colors={{
+                        text: pageColors.sortByText,
+                        background: pageColors.sortByBackground
+                    }}
                 />
 
                 <div>
-                    <ol className="Peop__LOP__List">
-                        {
-                            sortBy(users, activeSorting).map((user, index) => {
-                                return (
-                                    <ListRow
-                                        key={nanoid()}
-                                        user={user}
-                                        listOrder={index + 1}
-                                        self={user.id === 1 ? true : false}
-                                    />
-                                )
-                            })
-                        }
-                    </ol>
+                    {users.length === 0
+                        ? <p>No users found.</p>
+                        : (
+                            <ol className="Peop__LOP__List">
+                                {
+                                    sortBy(users, activeSorting).map((user, index) => {
+                                        return (
+                                            <ListRow
+                                                key={user.id}
+                                                user={user}
+                                                listOrder={index + 1}
+                                                self={user.id === Number(session?.user.id)}
+                                            />
+                                        )
+                                    })
+                                }
+                            </ol>
+                        )}
                 </div>
 
             </div>
-        </main>
+        </main >
     )
 }
