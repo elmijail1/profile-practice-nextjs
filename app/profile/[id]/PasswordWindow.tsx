686ec27569c7a1a715d303a8bdb8c8a24b6ce1aa
chalupa-bazooka <mteplyshev@gmail.com>
2025-05-13 16:39:07 +0400

    Add a new password setter

--- app/profile/[id]/PasswordWindow.tsx
+++ app/profile/[id]/PasswordWindow.tsx
@@ -10,6 +10,9 @@ type PasswordDisplayType = "password" | "text"
 
 export default function PasswordWindow({ setPasswordWindowOpen }: PropsFromProfilePage) {
 
+
+    const [progressStage, setProgressStage] = useState<"compare" | "setnew" | "success">("compare")
+
     const [currentPasswordInput, setCurrentPasswordInput] = useState("")
     const [passwordDisplay, setPasswordDisplay] = useState<PasswordDisplayType>("password")
     const [passwordIsWrong, setPasswordIsWrong] = useState<boolean>(false)
@@ -18,6 +21,9 @@ export default function PasswordWindow({ setPasswordWindowOpen }: PropsFromProfi
     const [newPasswordInput, setNewPasswordInput] = useState("")
     const [repeatPasswordInput, setRepeatPasswordInput] = useState("")
     const [passwordsMatch, setPasswordsMatch] = useState(false)
+
+    const [passwordChangeError, setPasswordChangeError] = useState(false)
+    const [passwordChangeSuccess, setPasswordChangeSuccess] = useState(false)
 
     useEffect(() => {
         setPasswordsMatch(newPasswordInput === repeatPasswordInput)
@@ -28,20 +34,62 @@ export default function PasswordWindow({ setPasswordWindowOpen }: PropsFromProfi
     ) {
         event.preventDefault()
 
-        const res = await fetch("/api/users/check-password", {
-            method: "POST",
-            body: JSON.stringify({ password: currentPasswordInput }),
-            headers: {
-                "Content-Type": "application/json"
+        try {
+
+            const res = await fetch("/api/users/check-password", {
+                method: "POST",
+                body: JSON.stringify({ password: currentPasswordInput }),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+
+            const result = await res.json()
+
+            if (!result.success) {
+                setPasswordIsWrong(true)
+                return
             }
-        })
+            setProgressStage("setnew")
+            setPasswordChangeError(false)
+        } catch (error) {
+            console.error("Network or server error: ", error)
+            setPasswordChangeError(true)
+        }
+    }
 
-        const result = await res.json()
+    const [newAndCurrentAreIdentical, setNewAndCurrentAreIdentical] = useState(false)
 
-        if (!result.success) {
-            setPasswordIsWrong(true)
-        } else {
-            setCurrentPasswordChecked(true)
+    useEffect(() => {
+        (newPasswordInput.length > 1 && currentPasswordInput === newPasswordInput) ? setNewAndCurrentAreIdentical(true) : setNewAndCurrentAreIdentical(false)
+    }, [newPasswordInput])
+
+    async function setNewPassword(
+        event: React.FormEvent<HTMLFormElement>
+    ) {
+        event.preventDefault()
+
+        try {
+            const res = await fetch("/api/account/password", {
+                method: "PATCH",
+                body: JSON.stringify({
+                    currentPassword: currentPasswordInput,
+                    newPassword: newPasswordInput
+                }),
+                headers: { "Content-Type": "application/json" }
+            })
+
+            const result = await res.json()
+
+            if (!result.success) {
+                console.error("Password change failed: ", result.message)
+                setPasswordChangeError(true)
+                return
+            }
+            setProgressStage("success")
+        } catch (error) {
+            console.error("Network or server error: ", error)
+            setPasswordChangeError(true)
         }
     }
 
@@ -58,77 +106,22 @@ export default function PasswordWindow({ setPasswordWindowOpen }: PropsFromProfi
             <h2>Change Password</h2>
 
             {
-                currentPasswordChecked
-                    ? <p className="text-black text-lg">1. Password checked successfully</p>
-                    :
-                    <form
-                        onSubmit={(event) => comparePasswords(event)}
-                        className="w-[90%] text-lg flex flex-col items-center text-cneter"
-                    >
-                        <label htmlFor="current-password" className="text-black">
-                            1. Enter your current password
-                            <input
-                                id="current-password"
-                                type={passwordDisplay}
-                                name="password"
-                                value={currentPasswordInput}
-                                onChange={(event) => setCurrentPasswordInput(event.target.value)}
-                                className="w-full text-gray-700 px-4 text-center text-2xl my-1"
-                                placeholder="Enter here"
-                                autoComplete="current-password"
-                            />
-                        </label>
-
-                        <button
-                            type="button"
-                            className="text-gray-600 text-sm my-3"
-                            onClick={() => setPasswordDisplay(prevDisplay => prevDisplay === "password" ? "text" : "password")}
-                        >
-                            Show password
-                        </button>
-                        {
-                            passwordIsWrong &&
-                            <p className="text-gray-600 text-sm my-3">
-                                Password is wrong, try again
-                            </p>
-                        }
-
-                        <button
-                            type="submit"
-                            className="border-2 border-black text-black w-[60%] rounded-2xl"
-                        >
-                            Check
-                        </button>
-                    </form>
-            }
-            {
-                currentPasswordChecked &&
-                <form className="w-[90%] text-lg flex flex-col items-center text-center">
+                progressStage === "compare" &&
+                <form
+                    onSubmit={(event) => comparePasswords(event)}
+                    className="w-[90%] text-lg flex flex-col items-center text-cneter"
+                >
                     <label htmlFor="current-password" className="text-black">
-                        2. Enter a new password
+                        1. Enter your current password
                         <input
-                            id="new-password"
+                            id="current-password"
                             type={passwordDisplay}
-                            name="password"
-                            value={newPasswordInput}
-                            onChange={(event) => setNewPasswordInput(event.target.value)}
+                            name="current-password"
+                            value={currentPasswordInput}
+                            onChange={(event) => setCurrentPasswordInput(event.target.value)}
                             className="w-full text-gray-700 px-4 text-center text-2xl my-1"
                             placeholder="Enter here"
-                            autoComplete="new-password"
-                        />
-                    </label>
-
-                    <label htmlFor="current-password" className="text-black">
-                        3. Repeat the new password
-                        <input
-                            id="repeat-password"
-                            type={passwordDisplay}
-                            name="password"
-                            value={repeatPasswordInput}
-                            onChange={(event) => setRepeatPasswordInput(event.target.value)}
-                            className="w-full text-gray-700 px-4 text-center text-2xl my-1"
-                            placeholder="Enter here"
-                            autoComplete="new-password"
+                            autoComplete="current-password"
                         />
                     </label>
 
@@ -137,24 +130,121 @@ export default function PasswordWindow({ setPasswordWindowOpen }: PropsFromProfi
                         className="text-gray-600 text-sm my-3"
                         onClick={() => setPasswordDisplay(prevDisplay => prevDisplay === "password" ? "text" : "password")}
                     >
-                        Show passwords
+                        {passwordDisplay === "password" ? "Show" : "Hide"} password
                     </button>
-
                     {
-                        newPasswordInput.length > 0 && repeatPasswordInput.length > 0 &&
+                        passwordIsWrong &&
                         <p className="text-gray-600 text-sm my-3">
-                            Passwords {!passwordsMatch && "don't"} match
+                            Password is wrong, try again
+                        </p>
+                    }
+                    {
+                        passwordChangeError &&
+                        <p className="text-red-700 my-3">
+                            Some mistake happened, try again!
                         </p>
                     }
 
                     <button
                         type="submit"
-                        className="border-2 border-black text-black w-[70%] rounded-2xl disabled:bg-gray-200 disabled:text-gray-500 disabled:border-0"
-                        disabled={!passwordsMatch}
+                        className="border-2 border-black text-black w-[60%] rounded-2xl"
                     >
-                        Update Password
+                        Check
                     </button>
                 </form>
+            }
+            {
+                progressStage === "setnew" &&
+                <>
+                    <p className="text-black text-lg">1. Password checked successfully</p>
+                    <form
+                        onSubmit={setNewPassword}
+                        className="w-[90%] text-lg flex flex-col items-center text-center"
+                    >
+                        <label htmlFor="new-password" className="text-black">
+                            2. Enter a new password
+                            <input
+                                id="new-password"
+                                type={passwordDisplay}
+                                name="new-password"
+                                value={newPasswordInput}
+                                onChange={(event) => setNewPasswordInput(event.target.value)}
+                                className="w-full text-gray-700 px-4 text-center text-2xl my-1"
+                                placeholder="Enter here"
+                                autoComplete="new-password"
+                            />
+                        </label>
+
+                        <label htmlFor="repeat-password" className="text-black">
+                            3. Repeat the new password
+                            <input
+                                id="repeat-password"
+                                type={passwordDisplay}
+                                name="repeat-password"
+                                value={repeatPasswordInput}
+                                onChange={(event) => setRepeatPasswordInput(event.target.value)}
+                                className="w-full text-gray-700 px-4 text-center text-2xl my-1
+                                disabled:bg-gray-300"
+                                placeholder="Enter here"
+                                autoComplete="new-password"
+                                disabled={newAndCurrentAreIdentical}
+                            />
+                        </label>
+
+                        <button
+                            type="button"
+                            className="text-gray-600 text-sm my-3"
+                            onClick={() => setPasswordDisplay(prevDisplay => prevDisplay === "password" ? "text" : "password")}
+                        >
+                            {passwordDisplay === "password" ? "Show" : "Hide"} passwords
+                        </button>
+
+                        {
+                            newPasswordInput.length > 0 && repeatPasswordInput.length > 0 &&
+                            <p className="text-gray-600 text-sm my-3">
+                                Passwords {!passwordsMatch && "don't"} match
+                            </p>
+                        }
+
+                        {
+                            passwordChangeError &&
+                            <p className="text-red-700 my-3">
+                                Some mistake happened, try again!
+                            </p>
+                        }
+
+                        {
+                            newAndCurrentAreIdentical &&
+                            <p className="text-red-700 my-3">
+                                New password can't be identical to the current one!
+                            </p>
+                        }
+
+                        <button
+                            type="submit"
+                            className="border-2 border-black text-black w-[70%] rounded-2xl disabled:bg-gray-200 disabled:text-gray-500 disabled:border-0"
+                            disabled={!passwordsMatch || newAndCurrentAreIdentical}
+                        >
+                            Update Password
+                        </button>
+                    </form>
+                </>
+            }
+            {
+                progressStage === "success" &&
+                <div
+                    className="w-[90%] text-lg flex flex-col items-center text-center gap-5"
+
+                >
+                    <p className="text-black">Successfully changed password!</p>
+                    <button
+                        onClick={() => setPasswordWindowOpen(false)}
+                        className="border-1 border-gray-500 text-black w-[70%] rounded-2xl disabled:bg-gray-200 disabled:text-gray-500 disabled:border-0"
+
+                    >
+                        Ok, close the window
+                    </button>
+                </div>
             }
 
 
