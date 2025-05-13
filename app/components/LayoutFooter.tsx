3cacc8b4baffa2d4cd3a6a5a7a7798fba4c8b881
chalupa-bazooka <mteplyshev@gmail.com>
2025-05-13 18:52:05 +0400

    Add more scenarios
    - Profile: unauthorized
    - Profile: authorized & your own profile
    - Profile: authorized & other profile

--- app/components/LayoutFooter.tsx
+++ app/components/LayoutFooter.tsx
@@ -7,7 +7,13 @@ import { signOut, useSession } from "next-auth/react";
 export default function LayoutFooter() {
 
     const pathname = usePathname()
+    const currentPathId = pathname?.split("/")[2]
+
     const { data: session, status } = useSession()
+
+    const isAuthenticated = !!session
+    const isOwnProfile = session?.user?.id?.toString() === currentPathId
+    const isOtherProfile = isAuthenticated && !isOwnProfile
 
     function isActive(route: string) {
         return pathname === route || pathname.includes(route)
@@ -88,8 +94,53 @@ export default function LayoutFooter() {
                     </>
                 )}
 
-                {/* Profile Page */}
-                {pathname.includes("/profile") && (
+                {/* Profile Page, unauthorized */}
+                {pathname.startsWith("/profile") && !isAuthenticated && (
+                    <>
+                        <Link
+                            href="/login"
+                            className="footer__link"
+                            style={isActive("/login") ? { backgroundColor: "hsl(200,50%,90%)" } : undefined}
+                        >
+                            Login
+                        </Link>
+                        <Link
+                            href="/people"
+                            className="footer__link"
+                            style={isActive("/people") ? { backgroundColor: "hsl(200,50%,90%)" } : undefined}
+                        >
+                            People
+                        </Link>
+                    </>
+                )}
+
+                {/* Profile Page, authorized + not yours */}
+                {pathname.startsWith("/profile") && isAuthenticated && isOtherProfile && (
+                    <>
+                        <button
+                            onClick={logoutUser}
+                            className="footer__link"
+                        >
+                            Logout
+                        </button>
+                        <Link
+                            href={`/profile/${session.user.id}`}
+                            className="footer__link"
+                        >
+                            My Profile
+                        </Link>
+                        <Link
+                            href="/people"
+                            className="footer__link"
+                            style={isActive("/people") ? { backgroundColor: "hsl(200,50%,90%)" } : undefined}
+                        >
+                            People
+                        </Link>
+                    </>
+                )}
+
+                {/* Profile Page, authorized + yours */}
+                {pathname.startsWith("/profile") && isAuthenticated && isOwnProfile && (
                     <>
                         <button
                             onClick={logoutUser}
@@ -106,6 +157,7 @@ export default function LayoutFooter() {
                         </Link>
                     </>
                 )}
+
             </div>
         </div>
     )
