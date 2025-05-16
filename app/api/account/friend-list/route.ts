8e0c6ed775fcc78ce25375f557e7e975391150c4
chalupa-bazooka <mteplyshev@gmail.com>
2025-05-16 11:07:29 +0400

    Ad a try-catch block

--- app/api/account/friend-list/route.ts
+++ app/api/account/friend-list/route.ts
@@ -4,24 +4,28 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    // 1. extract the user's ID from the session
-    const session = await getServerSession(authOptions)
-    if (!session?.user?.id) {
-        return NextResponse.json(null, { status: 204 })
-    }
+    try {
+        // 1. extract the user's ID from the session
+        const session = await getServerSession(authOptions)
+        if (!session?.user?.id) {
+            return NextResponse.json(null, { status: 204 })
+        }
 
-    // 2. extract the user's friends array from the server with the ID
-    const user = await prisma.user.findUnique({
-        where: { id: Number(session.user.id) },
-        select: { friends: true }
-    })
-    if (!user) {
-        return NextResponse.json(
-            { error: "Authenticated user not found in database" },
-            { status: 500 }
-        )
-    }
+        // 2. extract the user's friends array from the server with the ID
+        const user = await prisma.user.findUnique({
+            where: { id: Number(session.user.id) },
+            select: { friends: true }
+        })
+        if (!user) {
+            return NextResponse.json(
+                { error: "Authenticated user not found in database" },
+                { status: 500 }
+            )
+        }
 
-    // 3. return the friends value from the user's DB entry
-    return NextResponse.json(user.friends)
+        // 3. return the friends value from the user's DB entry
+        return NextResponse.json(user.friends)
+    } catch (error) {
+        return NextResponse.json({ error: error }, { status: 500 })
+    }
 }
