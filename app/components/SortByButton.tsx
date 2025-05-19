0a95b880dc69d3de5c52f2b3b927640038452912
chalupa-bazooka <mteplyshev@gmail.com>
2025-05-19 20:38:30 +0400

    Translate styles to TW + regactor

--- app/components/SortByButton.tsx
+++ app/components/SortByButton.tsx
@@ -1,12 +1,12 @@
 "use client";
-import { useState, useRef } from "react"
+import React, { useState, useRef } from "react"
 import { nanoid } from "nanoid";
 import { changeSorting, determineSortingButtonName } from "../../utilities/sorting";
 import useHandleElsewhereClick from "../../utilities/useHandleElsewhereClick";
 
-type PeopleProps = {
+type SortByButtonProps = {
     activeSorting: "joinedIn" | "name",
-    setActiveSorting: any, // how do you annotate a state setter?
+    setActiveSorting: React.Dispatch<React.SetStateAction<"joinedIn" | "name">>,
     sortingOptionsData: { name: string, displayName: string }[],
     colors: { text: number[], background: number[] }
 }
@@ -16,38 +16,41 @@ export default function SortByButton({
     setActiveSorting,
     sortingOptionsData,
     colors
-}: PeopleProps) {
+}: SortByButtonProps) {
 
     const [sortByMenuOpen, setSortByMenuOpen] = useState(false)
     let sortingMenuRef = useRef<any>(null)
     useHandleElsewhereClick(sortingMenuRef, "SBB__Button", setSortByMenuOpen)
 
-    return (
-        <div className="SBB__DivGen">
+    function isBlack(colors: number[]) {
+        const isBlack = colors[0] === 0
+            && colors[1] === 0
+            && colors[2] === 100
+        return isBlack ? "white" : "black"
+    }
 
-            <p
-                style={{
-                    color: colors.background[0] === 0 && colors.background[1] === 0 && colors.background[2] === 100
-                        ? "white"
-                        : "black"
-                }}
-            >
-                Sort by:
-            </p>
+    function arrayToHSLA(colors: number[], opacity: number = 1) {
+        return `hsla(${colors[0]}, ${colors[1]}%, ${colors[2]}%, ${opacity})`
+    }
+
+    return (
+        <div className="flex items-center gap-[0.2rem] relative text-[1.1rem] font-bold">
+
+            <p style={{ color: isBlack(colors.background) }}>Sort by:</p>
 
             <button
-                className="SBB__Button"
-                style={{ color: `hsl(${colors.text[0]}, ${colors.text[1]}%, ${colors.text[2]}%)`, backgroundColor: `hsl(${colors.background[0]}, ${colors.background[1]}%, ${colors.background[2]}%)` }}
+                className="px-[0.5rem] py-[0.2rem] rounded-[1rem] border-none font-bold"
+                style={{ color: arrayToHSLA(colors.text), backgroundColor: arrayToHSLA(colors.background) }}
                 onClick={() => setSortByMenuOpen(prevMenu => !prevMenu)}
             >
                 {determineSortingButtonName(sortingOptionsData, activeSorting)}
-                <span className="SBB__ButtonArrow">▼</span>
+                <span className="text-[0.6rem] ml-[0.3rem]">▼</span>
             </button>
 
             {sortByMenuOpen &&
                 <ul
-                    className="SBB__OptionsList"
-                    style={{ color: `hsla(${colors.text[0]}, ${colors.text[1]}%, ${colors.text[2]}%, 0.7)`, backgroundColor: `hsl(${colors.background[0]}, ${colors.background[1]}%, ${colors.background[2]}%)` }}
+                    className="bg-white font-medium list-none p-[1rem] rounded-[0.8rem] flex flex-col justify-start items-center gap-[0.8rem] absolute z-[1] top-[1.8rem] left-[4rem]"
+                    style={{ color: arrayToHSLA(colors.text, 0.7), backgroundColor: arrayToHSLA(colors.background) }}
                     ref={sortingMenuRef}
                 >
                     {
@@ -56,8 +59,8 @@ export default function SortByButton({
                                 <li
                                     onClick={() => changeSorting(sortingOption.name, setActiveSorting, setSortByMenuOpen(false))}
                                     key={nanoid()}
-                                    className={activeSorting === sortingOption.name ? "SBB__OptionItemActive" : ""}
-                                    style={activeSorting === sortingOption.name ? { color: `hsl(${colors.text[0]}, ${colors.text[1]}%, ${colors.text[2]}%)`, borderColor: `hsl(${colors.text[0]}, ${colors.text[1]}%, ${colors.text[2]}%)` } : undefined}
+                                    className={activeSorting === sortingOption.name ? "box-border font-semibold border-b-2" : ""}
+                                    style={activeSorting === sortingOption.name ? { color: arrayToHSLA(colors.text), borderColor: arrayToHSLA(colors.text) } : undefined}
                                 >
                                     {sortingOption.displayName}
                                 </li>
