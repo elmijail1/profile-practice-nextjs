bdb2c21512e7f73b1004e863171db3de71109084
chalupa-bazooka <mteplyshev@gmail.com>
2025-05-20 14:51:20 +0400

    Fix elsewhere click

--- utilities/useHandleElsewhereClick.ts
+++ utilities/useHandleElsewhereClick.ts
@@ -2,14 +2,27 @@ import { useEffect } from "react";
 
 export default function useHandleElsewhereClick(
     buttonRef: any,
-    buttonId: string,
-    setButtonActiveState: any
+    buttonId: string | string[],
+    setButtonActiveState: React.Dispatch<React.SetStateAction<boolean>>
 ) {
+
+    function clickingAtCurrentRef(event: any) {
+        return buttonRef.current?.contains(event.target)
+    }
+
+    function clickingAtThisButton(event: any) {
+        if (typeof buttonId === "string") {
+            return event.target.id === buttonId
+        } else if (Array.isArray(buttonId)) {
+            return buttonId.includes(event.target.id)
+        }
+    }
+
     useEffect(() => {
         function handleElsewhereClick(event: any) {
             if (
-                !buttonRef.current?.contains(event.target) &&
-                event.target.id !== buttonId
+                !clickingAtCurrentRef(event) &&
+                !clickingAtThisButton(event)
             ) {
                 setButtonActiveState(false);
             }
