1a3d745c4e493a544871bded85cfa0177e8d6326
chalupa-bazooka <mteplyshev@gmail.com>
2025-05-20 19:39:37 +0400

    Adapt for Profile buttons

--- app/components/WideButton.tsx
+++ app/components/WideButton.tsx
@@ -2,26 +2,34 @@ import React from "react"
 
 type WideButtonProps = {
     colors?: {
-        frontBG?: string
+        frontText?: string,
+        frontBG?: string,
         backBG?: string,
+        border?: string
     },
     disabledIf?: boolean,
+    onClick?: any,
     children: React.ReactNode
 }
 
 export default function WideButton(
-    { colors, disabledIf, children }: WideButtonProps
+    { colors, disabledIf, onClick, children }: WideButtonProps
 ) {
 
     const buttonFrontClass = "wide-button-front bg-white disabled:text-[hsl(265,50%,80%)] disabled:bg-[hsl(265,30%,60%)]"
     const buttonBackClass = "wide-button-front bg-[hsl(272,25%,55%)] absolute bottom-[-0.3rem] left-0 z-[-1]"
 
     return (
-        <div className="relative z-0 w-[90%] h-[3rem]">
+        <div className="relative z-0 w-[90%] h-[2.5rem]">
             <button
                 className={buttonFrontClass}
-                style={{ backgroundColor: colors?.frontBG }}
+                style={{
+                    color: colors?.frontText,
+                    backgroundColor: colors?.frontBG,
+                    border: colors?.border && `1px solid ${colors?.border}`
+                }}
                 disabled={disabledIf}
+                onClick={onClick ? onClick : null}
             >
                 {children}
             </button>
