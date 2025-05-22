6f7d5debe04368e0fd9a0a4c597982e69a491815
chalupa-bazooka <mteplyshev@gmail.com>
2025-05-22 20:21:16 +0400

    Create

---
+++ app/components/Loader.tsx
@@ -0,0 +1,11 @@
+export default function Loader() {
+    return (
+        <div className="h-[80vh] w-[100%] absolute flex items-center justify-center">
+            <p
+                className="text-xl font-semibold"
+            >
+                Loading...
+            </p>
+        </div>
+    )
+}
