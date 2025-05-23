721ee664c82e14bd36b09a6c5f7430207ffd036e
chalupa-bazooka <mteplyshev@gmail.com>
2025-05-23 11:16:32 +0400

    Fix the styles (absolute > fixed) & the id (moved it to the inner div)

--- app/profile/[id]/PopupWindow.tsx
+++ app/profile/[id]/PopupWindow.tsx
@@ -6,12 +6,12 @@ type TEFormProps = {
 export default function PopupWindow({ children, windowReference = null }: TEFormProps) { //*0.1
     return (
         <div
-            className="h-full w-full absolute top-[0] left-[0] flex justify-center items-start pt-[6.5rem] backdrop-filter backdrop-blur-[2px] bg-[hsla(130,_50%,_40%,_0.2)] z-20"
-            id="popup-window"
+            className="h-full w-full fixed top-[0] left-[0] flex justify-center items-start pt-[5rem] backdrop-filter backdrop-blur-[2px] bg-[hsla(130,_50%,_40%,_0.2)] z-20"
         >
             <div
                 className="w-72 px-[0] py-4 bg-[white] rounded-2xl flex flex-col items-center justify-center relative gap-8"
                 ref={windowReference ? windowReference : null}
+                id="popup-window"
             >
                 {children}
             </div>
