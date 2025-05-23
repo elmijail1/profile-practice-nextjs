ee8130f590e93c25683943198ece0e9a5bf350c1
chalupa-bazooka <mteplyshev@gmail.com>
2025-05-23 11:17:17 +0400

    Add user error messages + handler for 0 friends

--- app/profile/[id]/DataSection/ListOfFriends.tsx
+++ app/profile/[id]/DataSection/ListOfFriends.tsx
@@ -7,6 +7,7 @@ import ListRow from "./ListRow"
 // utilities
 import { sortBy } from "@/utilities/sorting"
 import { useProfileContext } from "@/lib/ProfileContext";
+import { useState } from "react";
 
 type FLWProps = {
     setProfileData: any,
@@ -21,6 +22,8 @@ export default function ListOfFriends({
 
     const { isOwnProfile } = useProfileContext()
 
+    const [error, setError] = useState("")
+
     async function removeFriend(friendId: number, mutate: () => void) {
         const res = await fetch("/api/account/friend-remove", {
             method: "PATCH",
@@ -29,6 +32,7 @@ export default function ListOfFriends({
         })
 
         if (!res.ok) {
+            setError("Removing friends in currently unavailable. Try again later.")
             throw new Error("Failed to remove friend")
         }
 
@@ -40,21 +44,34 @@ export default function ListOfFriends({
         await mutate()
     }
 
+    if (!friendsList || friendsList.length === 0) {
+        return (
+            <p className="text-black">
+                No friends found
+            </p>
+        )
+    }
+
     return (
-        <ol className="[list-style:none] p-0 mt-0 flex flex-col gap-2 w-4/5">
-            {sortBy(friendsList, "name").map((user, index) => {
-                return (
-                    <ListRow
-                        key={nanoid()}
-                        user={user}
-                        listOrder={index + 1}
-                        deleteOnClick={isOwnProfile ? () => removeFriend(user.id, mutate) : null}
-                        extraActionOnClick={() => setOpenFriendList(false)}
-                        textColor="0, 0%, 0%"
-                    />
-                )
-            })
-            }
-        </ol>
+        <>
+            <p className="text-red-500 w-[80%] text-center my-2">
+                {error}
+            </p>
+            <ol className="[list-style:none] p-0 mt-0 flex flex-col gap-2 w-4/5">
+                {sortBy(friendsList, "name").map((user, index) => {
+                    return (
+                        <ListRow
+                            key={nanoid()}
+                            user={user}
+                            listOrder={index + 1}
+                            deleteOnClick={isOwnProfile && !error ? () => removeFriend(user.id, mutate) : null}
+                            extraActionOnClick={() => setOpenFriendList(false)}
+                            textColor="0, 0%, 0%"
+                        />
+                    )
+                })
+                }
+            </ol>
+        </>
     )
 }
