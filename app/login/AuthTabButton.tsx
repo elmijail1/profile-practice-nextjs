ded67b189ccdec3a0135b7b73541c053172576f1
chalupa-bazooka <mteplyshev@gmail.com>
2025-05-24 19:24:00 +0400

    Add several xl styles

--- app/login/AuthTabButton.tsx
+++ app/login/AuthTabButton.tsx
@@ -10,13 +10,14 @@ export default function AuthTabButton(
     { isActive, onClick, children }: AuthTabButtonProps
 ) {
 
-    const baseClass = "w-1/2 h-full border-none bg-inherit text-white uppercase tracking-wider font-bold text-[1.1rem] cursor-pointer"
+    const tabClassMob = `w-1/2 h-full border-none bg-inherit text-white uppercase tracking-wider font-bold text-[1.1rem] cursor-pointer`
+    const tabClassDesk = `xl:max-w-[20rem] xl:max-h-[3.5rem] xl:rounded-2xl xl:border-white xl:border-solid xl:border-2`
+    const tabClassActive = `sm:inset-shadow-white-1 xl:border-b-4 xl:text-[1.2rem] ${children === "Log in" ? "xl:bg-[hsl(320,70%,70%)]" : "xl:bg-[hsl(260,70%,35%)]"}`
+
 
     return (
         <button
-            className={
-                `${baseClass} ${isActive ? "inset-shadow-white-1" : ""}`
-            }
+            className={`${tabClassMob} ${tabClassDesk} ${isActive && tabClassActive}`}
             onClick={onClick}
         >
             {children}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,13 +7,16 @@ import { useState } from "react"
 export default function Auth() {
     const [activeTab, setActiveTab] = useState<"login" | "signup">("login")
 
+    const tabButtonsMob = `h-20 w-full flex justify-center items-center border-solid border-b-[0.2rem] border-white fixed z-20 ${activeTab === "login" ? "bg-auth-login" : "bg-auth-signup"}`
+    const tabButtonsDesk = `xl:max-w-[30rem] xl:gap-[1rem] xl:items-end xl:border-none`
+
     return (
         <main>
             <div className={
-                `w-full min-h-[45rem] flex flex-col items-center relative pb-20 sm:w-[360px] sm:h-[600px]
+                `w-full min-h-max h-[calc(100vh-80px)] flex flex-col items-center relative pb-20
                 ${activeTab === "login" ? "bg-auth-login" : "bg-auth-signup"}`}
             >
-                <section className={`h-20 w-full flex justify-center items-center border-solid border-b-[0.2rem] border-white fixed z-20 sm:w-[360px] ${activeTab === "login" ? "bg-auth-login" : "bg-auth-signup"}`}>
+                <section className={`${tabButtonsMob} ${tabButtonsDesk}`}>
                     <AuthTabButton
                         isActive={activeTab === "login"}
                         onClick={() => setActiveTab("login")}
