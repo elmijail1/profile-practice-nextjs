ebc43f47d5d775639f79e20caba90752f71be02b
chalupa-bazooka <mteplyshev@gmail.com>
2025-05-24 15:32:09 +0400

    Add pagination support

--- app/profile/[id]/DataSection/useOwnFriendList.ts
+++ app/profile/[id]/DataSection/useOwnFriendList.ts
@@ -1,10 +1,14 @@
 import useSWR from "swr";
 
-async function fetchOwnFriends(friendList: number[]) {
+async function fetchOwnFriends(friendList: number[], page: number, limit: number) {
     const res = await fetch("/api/users/friend-list-data", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ friends: friendList })
+        body: JSON.stringify({
+            friends: friendList,
+            page: page || 1,
+            limit: limit || 5
+        })
     })
     if (!res.ok) {
         throw new Error("Failed to fetch")
@@ -12,13 +16,14 @@ async function fetchOwnFriends(friendList: number[]) {
     return await res.json()
 }
 
-export function useOwnFriendList(enabled: boolean, friendList: number[]) {
+export function useOwnFriendList(enabled: boolean, friendList: number[], page: number, limit: number) {
     const shouldFetch = enabled && friendList.length > 0
-    const key = shouldFetch ? ["/api/users/friend-list-data", friendList] : null
-    const swr = useSWR(key, () => fetchOwnFriends(friendList))
+    const key = shouldFetch ? ["/api/users/friend-list-data", friendList, page, limit] : null
+    const swr = useSWR(key, () => fetchOwnFriends(friendList, page, limit))
 
     return {
-        friendsSelf: swr.data,
+        friendsSelf: swr.data?.users,
+        totalSelf: swr.data?.total,
         isLoading: swr.isLoading,
         error: swr.error,
         mutate: swr.mutate
