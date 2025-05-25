1c732af8c19c5c80bfb2e5d40f4fd408916f9f9e
chalupa-bazooka <mteplyshev@gmail.com>
2025-05-25 11:35:22 +0400

    Activate inset-shadow-white-1 style

--- app/login/AuthTabButton.tsx
+++ app/login/AuthTabButton.tsx
@@ -14,7 +14,7 @@ export default function AuthTabButton(
 
     const tabClassMob = `w-1/2 h-full border-none bg-inherit text-white uppercase tracking-wider font-bold text-[1.1rem] cursor-pointer`
     const tabClassDesk = `xl:max-w-[20rem] xl:max-h-[3.5rem] xl:rounded-2xl xl:border-white xl:border-solid xl:border-2`
-    const tabClassActive = `sm:inset-shadow-white-1 xl:border-b-4 xl:text-[1.2rem] ${buttonType === "login" ? "xl:bg-[hsl(320,70%,70%)]" : "xl:bg-[hsl(260,70%,35%)]"}`
+    const tabClassActive = `inset-shadow-white-1 xl:border-b-4 xl:text-[1.2rem] ${buttonType === "login" ? "xl:bg-[hsl(320,70%,70%)]" : "xl:bg-[hsl(260,70%,35%)]"}`
     const tabClassInactive = `${buttonType === "login" ? "xl:hover:bg-[hsl(320,60%,65%)]" : "xl:hover:bg-[hsl(260,60%,30%)]"}`
 
 
