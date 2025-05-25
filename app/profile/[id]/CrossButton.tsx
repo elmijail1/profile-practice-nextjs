10d2b35935cc448bf10b3ad86e01429d15a79759
chalupa-bazooka <mteplyshev@gmail.com>
2025-05-25 20:48:57 +0400

    Tighten typing

--- app/profile/[id]/CrossButton.tsx
+++ app/profile/[id]/CrossButton.tsx
@@ -1,4 +1,6 @@
-export default function CrossButton({ onClickAction }: any) {
+import React from "react";
+
+export default function CrossButton({ onClickAction }: { onClickAction: React.MouseEventHandler<HTMLButtonElement> }) {
     return (
         <button
             className="text-[hsl(0,_0%,_70%)] text-[3rem] bg-none border-none [rotate:45deg] absolute top-[0] right-[0] w-12 h-12 flex justify-center items-center xl:cursor-pointer"
