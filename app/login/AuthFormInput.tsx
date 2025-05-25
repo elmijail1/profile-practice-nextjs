9f05e5366211a4bdd6c028a2e5a703bcfe5d764d
chalupa-bazooka <mteplyshev@gmail.com>
2025-05-25 12:57:32 +0400

    Add xl styles for login & footer

--- app/login/AuthFormInput.tsx
+++ app/login/AuthFormInput.tsx
@@ -22,7 +22,7 @@ export default function AuthFormInput(
     const id = `auth-${name}`
 
     const labelClass = "bg-white min-h-[6.5rem] w-[90%] rounded-[1rem] px-[1.5rem] pt-[1.5rem] pb-[0.5rem] flex flex-col items-start justify-start gap-[0.5rem] font-semibold text-[1.1rem] text-[hsl(0,0%,30%)] relative"
-    const inputClass = "  block w-full h-[1.8rem] rounded-[1rem] border-[2.5px] border-[hsl(0,0%,90%)] text-[1rem] px-[0.5rem]  pr-[2.4rem]"
+    const inputClass = "block w-full h-[1.8rem] rounded-[1rem] border-[2.5px] border-[hsl(0,0%,90%)] text-[1rem] px-[0.5rem]  pr-[2.4rem]"
 
     return (
         <label className={labelClass}>
