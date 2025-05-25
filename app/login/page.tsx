b0df4d3f1927b5809d002640379874a43001d423
chalupa-bazooka <mteplyshev@gmail.com>
2025-05-25 12:57:39 +0400

    Add xl styles for login & footer

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,15 +7,15 @@ import { useState } from "react"
 export default function Auth() {
     const [activeTab, setActiveTab] = useState<"login" | "signup">("login")
 
+    const pageClassMob = `w-full min-h-max h-[calc(100vh-80px)] flex flex-col items-center relative pb-20 ${activeTab === "login" ? "bg-auth-login" : "bg-auth-signup"}`
+    const pageClassDesk = `xl:h-screen xl:pt-[3rem] xl:pb-5`
+
     const tabButtonsMob = `h-20 w-full flex justify-center items-center border-solid border-b-[0.2rem] border-white fixed z-20 ${activeTab === "login" ? "bg-auth-login" : "bg-auth-signup"}`
-    const tabButtonsDesk = `xl:max-w-[30rem] xl:gap-[1rem] xl:items-end xl:border-none`
+    const tabButtonsDesk = `xl:max-w-[30rem] xl:gap-[1rem] xl:items-end xl:border-none xl:absolute`
 
     return (
         <main>
-            <div className={
-                `w-full min-h-max h-[calc(100vh-80px)] flex flex-col items-center relative pb-20
-                ${activeTab === "login" ? "bg-auth-login" : "bg-auth-signup"}`}
-            >
+            <div className={`${pageClassMob} ${pageClassDesk}`}>
                 <section className={`${tabButtonsMob} ${tabButtonsDesk}`}>
                     <AuthTabButton
                         isActive={activeTab === "login"}
