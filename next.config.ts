512b2e9c2b4a60a16c1662cae42f959e1dbcc9f2
chalupa-bazooka <mteplyshev@gmail.com>
2025-05-28 11:03:22 +0400

    Revert all the Vercel-focused changes as I won't use Vercel for deployment now

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
 import type { NextConfig } from "next";
-const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
 
 const nextConfig: NextConfig = {
-  /** Fix prisma client issue when deploying to Vercel */
-  webpack: (config, { isServer }) => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-    if (isServer) config.plugins = [...config.plugins, new PrismaPlugin()]
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return config
-  },
   async redirects() {
     return [
       {
