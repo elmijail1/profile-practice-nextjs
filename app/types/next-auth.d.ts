fc923140f87df3472198014ffe7c9e243810bb5a
chalupa-bazooka <mteplyshev@gmail.com>
2025-05-31 20:28:10 +0400

    Define the lastActive property of the JWT type

--- app/types/next-auth.d.ts
+++ app/types/next-auth.d.ts
@@ -1,4 +1,14 @@
-import { DefaultSession, DefaultUser } from "next-auth"
+import NextAuth, { DefaultSession, DefaultUser } from "next-auth"
+import { JWT } from "next-auth/jwt"
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        lastActive?: number;
+        id?: string;
+        name?: string;
+        email?: string;
+    }
+}
 
 declare module "next-auth" {
     interface Session {
