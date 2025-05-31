3c49b833c994f43fb458368ec355bf6c06dc8992
chalupa-bazooka <mteplyshev@gmail.com>
2025-05-31 12:54:38 +0400

    Add desktop styles

--- app/components/ErrorPopup.tsx
+++ app/components/ErrorPopup.tsx
@@ -8,18 +8,21 @@ type ErrorProps = {
 
 export default function ErrorPopup(
     { error, setError, windowReference }: ErrorProps) {
+
+    const backgroundClass = "h-full w-full fixed top-[0] left-[0] flex justify-center items-center backdrop-filter backdrop-blur-[2px] bg-[hsl(0,0%,0%,0.3)] z-20"
+    const popupClass = "w-[90%] xl:max-w-[30rem] bg-red-400 py-4 px-4 text-white font-semibold rounded-xl text-center flex flex-col items-center gap-2 mb-[4rem]"
+    const buttonClass = "rounded-xl px-2 py-1 bg-white text-red-400 w-[50%] xl:hover:cursor-pointer xl:hover:brightness-90"
+
     return (
-        <div
-            className="h-full w-full fixed top-[0] left-[0] flex justify-center items-center backdrop-filter backdrop-blur-[2px] bg-[hsl(0,0%,0%,0.3)] z-20"
-        >
+        <div className={backgroundClass}>
             <div
-                className="w-[90%] bg-red-400 py-4 px-4 text-white font-semibold rounded-xl text-center flex flex-col items-center gap-2 mb-[4rem]"
+                className={popupClass}
                 ref={windowReference ? windowReference : null}
                 id="popup-window-error"
             >
                 <p>{error}</p>
                 <button
-                    className="rounded-xl px-2 py-1 bg-white text-red-400 w-[50%]"
+                    className={buttonClass}
                     onClick={() => setError("")}
                 >
                     Close
