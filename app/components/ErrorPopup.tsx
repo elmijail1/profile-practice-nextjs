3d94766522c7481d91ec323c7cfd8b14e40e72ae
chalupa-bazooka <mteplyshev@gmail.com>
2025-06-04 11:32:02 +0400

    Add conditional typing

--- app/components/ErrorPopup.tsx
+++ app/components/ErrorPopup.tsx
@@ -1,13 +1,15 @@
 import React, { SetStateAction } from "react"
 
-type ErrorProps = {
+type SetError<T> = React.Dispatch<React.SetStateAction<T>>
+
+type ErrorProps<T extends boolean | string> = {
     error: string,
-    setError: React.Dispatch<SetStateAction<string>>,
+    setError: SetError<T>,
     windowReference?: React.Ref<HTMLDivElement> | null
 }
 
-export default function ErrorPopup(
-    { error, setError, windowReference }: ErrorProps) {
+export default function ErrorPopup<T extends boolean | string>(
+    { error, setError, windowReference }: ErrorProps<T>) {
 
     const backgroundClass = "h-full w-full fixed top-[0] left-[0] flex justify-center items-center backdrop-filter backdrop-blur-[2px] bg-[hsl(0,0%,0%,0.3)] z-20"
     const popupClass = "w-[90%] xl:max-w-[30rem] bg-red-400 py-4 px-4 text-white font-semibold rounded-xl text-center flex flex-col items-center gap-2 mb-[4rem]"
@@ -23,7 +25,13 @@ export default function ErrorPopup(
                 <p>{error}</p>
                 <button
                     className={buttonClass}
-                    onClick={() => setError("")}
+                    onClick={() => {
+                        if (typeof (false as T) === "boolean") {
+                            setError(false as T)
+                        } else if (typeof ("" as T) === "string") {
+                            setError("" as T)
+                        }
+                    }}
                 >
                     Close
                 </button>
