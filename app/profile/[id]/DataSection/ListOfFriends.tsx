c30a925da08d2729af322f530444adbb091c1996
chalupa-bazooka <mteplyshev@gmail.com>
2025-06-06 20:32:06 +0400

    Add isDeleting indicator
    - for user to better undersand that the deletion is underway
    - the deleted friend's name is highlighted with red
    - deletion is unavailable during isDeleting

--- app/profile/[id]/DataSection/ListOfFriends.tsx
+++ app/profile/[id]/DataSection/ListOfFriends.tsx
@@ -24,9 +24,13 @@ export default function ListOfFriends({
     const { isOwnProfile } = useProfileContext()
 
     const [error, setError] = useState("")
+    const [isDeleting, setIsDeleting] = useState(false)
+    const [whosDeleted, setWhosDeleted] = useState<number | null>(null)
     const router = useRouter()
 
     async function removeFriend(friendId: number, mutate?: () => Promise<User | undefined>) {
+        setWhosDeleted(friendId)
+        setIsDeleting(true)
         const res = await fetch("/api/account/friend-remove", {
             method: "PATCH",
             body: JSON.stringify({ friendId }),
@@ -41,6 +45,7 @@ export default function ListOfFriends({
                 return
             }
             setError("Removing friends in currently unavailable. Try again later.")
+            setIsDeleting(false)
             throw new Error("Failed to remove friend")
         }
 
@@ -60,6 +65,8 @@ export default function ListOfFriends({
         if (mutate) {
             await mutate()
         }
+        setWhosDeleted(null)
+        setIsDeleting(false)
     }
 
     if (!friendsList || friendsList.length === 0) {
@@ -72,21 +79,31 @@ export default function ListOfFriends({
 
     return (
         <>
-            <p className="text-red-500 w-[80%] text-center my-1">
-                {error}
-            </p>
-            <ol className="[list-style:none] p-0 mt-0 flex flex-col gap-2 w-[14rem] h-full">
+            {error &&
+                <p className="text-red-500 w-[100%] text-center my-1">
+                    {error}
+                </p>
+            }
+            <ol className="[list-style:none] p-0 mt-0 flex flex-col gap-2 w-[14rem] h-full relative">
                 {sortBy(friendsList, "name").map((user, index) => {
                     return (
                         <ListRow
                             key={nanoid()}
                             user={user}
                             listOrder={((page - 1) * limit) + index + 1}
-                            deleteOnClick={isOwnProfile && !error ? () => removeFriend(user.id, mutate) : null}
-                            textColor="0, 0%, 0%"
+                            deleteOnClick={isOwnProfile && !isDeleting && !error ? () => removeFriend(user.id, mutate) : null}
+                            textColor={whosDeleted === user.id ? "0, 100%, 50%" : "0, 0%, 0%"}
                         />
                     )
                 })
+                }
+                {
+                    isDeleting &&
+                    <div className="w-full h-full absolute rotate-90 flex items-start justify-start animate-pulse">
+                        <p className="text-black font-semibold tracking-wider text-[1.1rem] w-content uppercase mt-2 ml-[-1rem]">
+                            Deleting...
+                        </p>
+                    </div>
                 }
             </ol>
         </>
